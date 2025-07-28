d4147a58bf160868345de39d392eeb6828011648
Michael Grishchenko <mikigym19@gmail.com>
2025-07-28 18:19:54 +0300

    add router for swagger docs

--- internal/delivery/api/sub/router.go
+++ internal/delivery/api/sub/router.go
@@ -1,11 +1,21 @@
 package sub
 
-import "github.com/go-chi/chi/v5"
+import (
+	"github.com/go-chi/chi/v5"
+	_ "github.com/maYkiss56/subscription-aggregation-service/docs"
+	httpSwagger "github.com/swaggo/http-swagger"
+)
 
 func NewRouter(subs *HandlerSub) chi.Router {
 	r := chi.NewRouter()
 
+	// Swagger
+	r.Get("/swagger/*", httpSwagger.Handler(
+		httpSwagger.URL("/swagger/doc.json"),
+	))
+
 	r.Route("/api/subs", func(r chi.Router) {
+
 		r.Get("/", subs.GetAllSubs)
 		r.Get("/{user_id}", subs.GetSubByUserID)
 		r.Post("/total", subs.CalculateTotalCost)
