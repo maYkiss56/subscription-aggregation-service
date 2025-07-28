c3be4d5d307b2109cf931ea2d15985e85c454ebc
Michael Grishchenko <mikigym19@gmail.com>
2025-07-28 18:21:34 +0300

    annotations type for swagger and fix type for date

--- internal/domain/dto.go
+++ internal/domain/dto.go
@@ -1,33 +1,62 @@
 package domain
 
 import (
-	"time"
-
 	"github.com/google/uuid"
+	"github.com/maYkiss56/subscription-aggregation-service/internal/utils"
 )
 
+// CreateSubRequest represents request to create subscription
 type CreateSubRequest struct {
-	ServiceName string     `json:"service_name"`
-	Price       int        `json:"price"`
-	UserID      uuid.UUID  `json:"user_id"`
-	StartDate   time.Time  `json:"start_date"`
-	EndDate     *time.Time `json:"end_date,omitempty"`
+	ServiceName string    `json:"service_name" example:"Netflix"`
+	Price       int       `json:"price" example:"1000"`
+	UserID      uuid.UUID `json:"user_id" example:"550e8400-e29b-41d4-a716-446655440000"`
+	StartDate   string    `json:"start_date" example:"07-2025"`
+	EndDate     string    `json:"end_date" example:"07-2025"`
 }
 
-type CreateSubResponse struct {
-	ID uuid.UUID `json:"id"`
-}
-
+// UpdateSubRequest represents request to update subscription
 type UpdateSubRequest struct {
-	ServiceName *string    `json:"service_name"`
-	Price       *int       `json:"price"`
-	StartDate   *time.Time `json:"start_date"`
-	EndDate     *time.Time `json:"end_date,omitempty"`
+	ServiceName *string `json:"service_name,omitempty" example:"Netflix Premium"`
+	Price       *int    `json:"price,omitempty" example:"1500"`
+	StartDate   *string `json:"start_date,omitempty" example:"07-2025"`
+	EndDate     *string `json:"end_date,omitempty" example:"07-2025"`
 }
 
+// TotalCostFilter represents filter for total cost calculation
 type TotalCostFilter struct {
-	UserID      *uuid.UUID `json:"user_id"`
-	ServiceName *string    `json:"service_name"`
-	StartPeriod time.Time  `json:"start_period"`
-	EndPeriod   time.Time  `json:"end_period"`
+	UserID      *uuid.UUID `json:"user_id,omitempty" example:"550e8400-e29b-41d4-a716-446655440000"`
+	ServiceName *string    `json:"service_name,omitempty" example:"Netflix"`
+	StartPeriod string     `json:"start_period" example:"07-2025"`
+	EndPeriod   string     `json:"end_period" example:"07-2025"`
+}
+
+// SubResponse представляет ответ с датами в формате MM-YYYY
+type SubResponse struct {
+	ID          uuid.UUID `json:"id"`
+	ServiceName string    `json:"service_name"`
+	Price       int       `json:"price"`
+	UserID      uuid.UUID `json:"user_id"`
+	StartDate   string    `json:"start_date"` // MM-YYYY
+	EndDate     string    `json:"end_date"`   // MM-YYYY
+}
+
+// convertSubToResponse преобразует доменную Sub в SubResponse
+func ConvertSubToResponse(sub *Sub) *SubResponse {
+	return &SubResponse{
+		ID:          sub.ID,
+		ServiceName: sub.ServiceName,
+		Price:       sub.Price,
+		UserID:      sub.UserID,
+		StartDate:   utils.ToMonthYearString(sub.StartDate),
+		EndDate:     utils.ToMonthYearString(sub.EndDate),
+	}
+}
+
+// convertSubsToResponse преобразует список доменных Sub в список SubResponse
+func ConvertSubsToResponse(subs []*Sub) []*SubResponse {
+	result := make([]*SubResponse, len(subs))
+	for i, sub := range subs {
+		result[i] = ConvertSubToResponse(sub)
+	}
+	return result
 }
--- internal/domain/sub.go
+++ internal/domain/sub.go
@@ -6,16 +6,17 @@ import (
 	"github.com/google/uuid"
 )
 
+// Sub represents subscription model
 type Sub struct {
-	ID          uuid.UUID
-	ServiceName string
-	Price       int
-	UserID      uuid.UUID
-	StartDate   time.Time
-	EndDate     *time.Time
+	ID          uuid.UUID `json:"id" example:"550e8400-e29b-41d4-a716-446655440000"`
+	ServiceName string    `json:"service_name" example:"Netflix"`
+	Price       int       `json:"price" example:"1000"`
+	UserID      uuid.UUID `json:"user_id" example:"550e8400-e29b-41d4-a716-446655440000"`
+	StartDate   time.Time `json:"start_date" example:"01-2023"`
+	EndDate     time.Time `json:"end_date" example:"01-2023"`
 }
 
-func New(serviceName string, price int, userID uuid.UUID, startDate time.Time, endDate *time.Time) (*Sub, error) {
+func New(serviceName string, price int, userID uuid.UUID, startDate time.Time, endDate time.Time) (*Sub, error) {
 	return &Sub{
 		ID:          uuid.New(),
 		ServiceName: serviceName,
@@ -24,8 +25,4 @@ func New(serviceName string, price int, userID uuid.UUID, startDate time.Time, e
 		StartDate:   startDate,
 		EndDate:     endDate,
 	}, nil
-}
-
-func (s *Sub) MonthYear() string {
-	return s.StartDate.Format("01-2006")
 }
